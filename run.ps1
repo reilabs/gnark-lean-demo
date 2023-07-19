2d4c3a95ea750ae958f31c6e0c862d7313c17385
Giuseppe <8973725+Eagle941@users.noreply.github.com>
2023-07-19 19:12:43 +0100

    Added lean demo files

--- run.ps1
+++ run.ps1
@@ -5,4 +5,4 @@ Write-Host "It's recommended to avoid PowerShell to prevent any symbol encoding 
 # Powershell uses different encoding which means it's struggling to append
 # math symbols to file
 go mod download
-go run go-circuit/semaphore.go > lean-circuit/GnarkLeanDemo.lean
+go run go-circuit/semaphore.go > lean-circuit/LeanCircuit.lean
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 go mod download
-go run go-circuit/semaphore.go > lean-circuit/GnarkLeanDemo.lean
+go run go-circuit/semaphore.go > lean-circuit/LeanCircuit.lean
