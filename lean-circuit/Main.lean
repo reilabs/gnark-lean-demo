c6cc99b2bee67c8401b425edd1585869c35beb7c
Giuseppe <8973725+Eagle941@users.noreply.github.com>
2023-07-25 17:14:23 +0100

    Refactoring for new proven-zk

--- lean-circuit/Main.lean
+++ lean-circuit/Main.lean
@@ -39,13 +39,17 @@ theorem same_hash_same_identity (IdentityNullifier₁ IdentityNullifier₂ Ident
         unfold identity_commitment at h
 
         have inps_same₁ := Fact.elim (inferInstance : Fact (perfect_hash dummy_hash₁)) h
-        have := congrArg Vector.head inps_same₁
+        have inps_same₁ := congrArg Vector.head inps_same₁
 
         unfold secret at inps_same₁
         have inps_same₂ := Fact.elim (inferInstance : Fact (perfect_hash dummy_hash₂)) inps_same₁
-        have := congrArg Vector.head inps_same₂
-        have := congrArg (Vector.head ∘ Vector.tail) inps_same₂
-        assumption
+        have h₁ := congrArg Vector.head inps_same₂
+        have h₂ := congrArg (Vector.head ∘ Vector.tail) inps_same₂
+        simp at h₁
+        simp at h₂
+        rw [h₁]
+        rw [h₂]
+        simp
     }
     case mpr => {
         intro h
