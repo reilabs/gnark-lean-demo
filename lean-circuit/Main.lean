409d32d74695974dbac665a93bf3e97f5ea717ba
Giuseppe <8973725+Eagle941@users.noreply.github.com>
2023-07-27 22:14:46 +0100

    wip

--- lean-circuit/Main.lean
+++ lean-circuit/Main.lean
@@ -10,16 +10,15 @@ abbrev Order := Semaphore.Order
 variable [Fact (Nat.Prime Order)]
 abbrev F := Semaphore.F
 
-def nat_to_dir : Nat -> Dir
+def nat_to_dir : Fin 2 -> Dir
     | 0 => Dir.right
     | 1 => Dir.left
-    | Nat.succ (Nat.succ _) => panic "Dir can be 0 or 1"
 
 def poseidon₁ : Hash F 1 := fun a => (Poseidon.perm Constants.x5_254_2 vec![0, a.get 0]).get 0
 def poseidon₂ : Hash F 2 := fun a => (Poseidon.perm Constants.x5_254_3 vec![0, a.get 0, a.get 1]).get 0
 
 def create_dir_vec {depth} (ix: Vector F depth) : Vector Dir depth :=
-    Vector.map nat_to_dir (Vector.map ZMod.val ix)
+    Vector.map nat_to_dir (Vector.map (fun v => v.val) ix)
 
 abbrev secret (IdentityNullifier: F) (IdentityTrapdoor: F) : F :=
     poseidon₂ vec![IdentityNullifier, IdentityTrapdoor]
@@ -27,7 +26,7 @@ abbrev secret (IdentityNullifier: F) (IdentityTrapdoor: F) : F :=
 abbrev identity_commitment (IdentityNullifier: F) (IdentityTrapdoor: F) : F :=
     poseidon₁ vec![(secret IdentityNullifier IdentityTrapdoor)]
 
-abbrev nullifier_hash (IdentityNullifier: F) (ExternalNullifier: F) : F :=
+abbrev nullifier_hash (ExternalNullifier: F) (IdentityNullifier: F) : F :=
     poseidon₂ vec![ExternalNullifier, IdentityNullifier]
 
 theorem same_hash_same_identity (IdentityNullifier₁ IdentityNullifier₂ IdentityTrapdoor₁ IdentityTrapdoor₂ : F)
@@ -47,20 +46,85 @@ def circuit_simpl (IdentityNullifier IdentityTrapdoor ExternalNullifier Nullifie
     NullifierHash = nullifier_hash ExternalNullifier IdentityNullifier ∧
     MerkleTree.recover poseidon₂ (create_dir_vec Path) Proof (identity_commitment IdentityNullifier IdentityTrapdoor) = Root
 
+lemma get_vec_0 {d : Nat} {a : Vector α (Nat.succ d)} : (Vector.get a 0) = a[0] := by rfl
+lemma get_vec_1 {d : Nat} {a : Vector α (Nat.succ (Nat.succ d))} : (Vector.get a 1) = a[1] := by rfl
+
+lemma head_is_get {d : Nat} {v : Vector α (Nat.succ d)} : (Vector.head v) = v[0] := by sorry
+
+lemma replace_get_1 {a b : F} : (Vector.get vec![a, b] 1) = b := by rfl
+
+theorem merkle_recover_correct (Leaf: F) (PathIndices: Vector F 3) (Siblings: Vector F 3) (k: F -> Prop):
+  Semaphore.MerkleTreeInclusionProof_3_3 Leaf PathIndices Siblings k = k (MerkleTree.recover poseidon₂ (create_dir_vec PathIndices) Siblings Leaf) := by
+    unfold Semaphore.MerkleTreeInclusionProof_3_3
+    unfold MerkleTree.recover
+    unfold Semaphore.Poseidon2
+    simp [poseidon_3_correct]
+    unfold poseidon₂
+    simp [get_vec_0]
+    simp [get_vec_1]
+    simp [head_is_get]
+
+    sorry
+
 lemma circuit_simplified {IdentityNullifier IdentityTrapdoor SignalHash ExternalNullifier NullifierHash Root: F} {Path Proof: Vector F 3}:
     Semaphore.circuit IdentityNullifier IdentityTrapdoor Path Proof SignalHash ExternalNullifier NullifierHash Root ↔
     circuit_simpl IdentityNullifier IdentityTrapdoor ExternalNullifier NullifierHash Root Path Proof := by
-    sorry
+    unfold Semaphore.circuit
+    unfold circuit_simpl
+    unfold Semaphore.Poseidon2
+    unfold Semaphore.Poseidon1
+    unfold nullifier_hash
+    unfold identity_commitment
+    unfold secret
+    unfold poseidon₂
+    unfold poseidon₁
+    unfold Gates.eq
+    simp [poseidon_3_correct, poseidon_2_correct, merkle_recover_correct]
+    unfold poseidon₂
+    simp [poseidon_2_correct, fold_vec_3]
+    apply Iff.intro
+    case _ => {
+        intro h
+        cases h
+        rename_i h₁ h₂
+        apply And.intro
+        case _ => {
+            rw [<-h₁]
+            simp [replace_get_1, head_is_get]
+            rfl
+        }
+        case _ => {
+            rw [<-h₂]
+            simp [replace_get_1, head_is_get]
+            rfl
+        }
+    }
+    case _ => {
+        intro h
+        cases h
+        rename_i h₁ h₂
+        apply And.intro
+        case _ => {
+            rw [h₁]
+            simp [replace_get_1, head_is_get]
+            rfl
+        }
+        case _ => {
+            rw [<-h₂]
+            simp [replace_get_1, head_is_get]
+            rfl
+        }
+    }
 
 theorem always_possible_to_signal
-    (IdentityNullifier IdentitityTrapdoor SignalHash ExtNullifier : F)
+    (IdentityNullifier IdentityTrapdoor SignalHash ExtNullifier : F)
     (Tree : MerkleTree F poseidon₂ 3)
     (Path : Vector F 3)
-    (comm_in_tree : Tree.item_at (create_dir_vec Path) = identity_commitment IdentityNullifier IdentitityTrapdoor)
+    (comm_in_tree : Tree.item_at (create_dir_vec Path) = identity_commitment IdentityNullifier IdentityTrapdoor)
     :
     Semaphore.circuit
         IdentityNullifier
-        IdentitityTrapdoor
+        IdentityTrapdoor
         Path
         (Tree.proof (create_dir_vec Path)) -- Siblings
         SignalHash
