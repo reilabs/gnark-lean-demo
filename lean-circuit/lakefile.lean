8bad40d771257a66512c5b388e361b2bb4a11e2b
Eagle941 <8973725+Eagle941@users.noreply.github.com>
2023-08-01 14:39:01 +0100

    chore: replaced dependencies folder with repos (#16)

--- lean-circuit/lakefile.lean
+++ lean-circuit/lakefile.lean
@@ -8,14 +8,12 @@ package «lean-circuit» {
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"@"ea67efc21e4e1496f0a1d954cd0c0a952523133a"
 
+require ProvenZK from git
+  "https://github.com/reilabs/proven-zk.git"@"v1.0.0"
+
 lean_lib LeanCircuit {
   -- add library configuration options here
 }
-
--- require mathlib from git
---   "https://github.com/reilabs/proven-zk.git"
-
-require ProvenZK from ".."/".."/"proven-zk"
 
 @[default_target]
 lean_exe «lean-circuit» {
