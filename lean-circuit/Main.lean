f2658e7a1956ec0ce3c8fc54df8062e5a449551d
Giuseppe <8973725+Eagle941@users.noreply.github.com>
2023-08-03 19:26:53 +0100

    Added some comments and linting

--- lean-circuit/Main.lean
+++ lean-circuit/Main.lean
@@ -10,7 +10,8 @@ open Semaphore (F Order)
 variable [Fact (Nat.Prime Order)]
 
 -- always_possible_to_signal proves the "No Censorship" property of the Semaphore protocol.
--- Given an identity already present in Tree, it is always possible to signal.
+-- Given an identity_commitment (identity hash) already present in Tree, it is always possible to signal.
+-- Path is the direction vector to walk from the root to the identity hash (leaf)
 theorem always_possible_to_signal
   (IdentityNullifier IdentityTrapdoor SignalHash ExtNullifier : F)
   (Tree : MerkleTree F poseidon₂ 20)
@@ -32,6 +33,9 @@ theorem always_possible_to_signal
     apply embed_dir_vector_is_binary
 
 -- signaller_is_in_tree proves the "No Unauthorized Signalling" property of the Semaphore protocol
+-- If the circuit is successful, then it means that the identity_commitment (identity hash) is a leaf
+-- of the Tree which means the identity has signed up.
+-- Path is the direction vector to walk from the identity hash (leaf) to the root
 theorem signaller_is_in_tree
     (IdentityNullifier IdentityTrapdoor SignalHash ExtNullifier NullifierHash : F)
     (Tree : MerkleTree F poseidon₂ 20)
@@ -46,6 +50,9 @@ theorem signaller_is_in_tree
     assumption
 
 -- no_double_signal_with_same_commitment proves the "No Double Signalling" property of the Semaphore protocol.
+-- If there are two signals with the same ExtNullifier and identity_commitment, then it means they must have
+-- the same NullifierHash which means the user has already submitted a signal because nullifier hash
+-- is made up of identity hash (identity_commitment) and external voting hash (ExtNullifier).
 theorem no_double_signal_with_same_commitment
     (IdentityNullifier₁ IdentityNullifier₂ IdentityTrapdoor₁ IdentityTrapdoor₂ SignalHash₁ SignalHash₂ ExtNullifier₁ ExtNullifier₂ NullifierHash₁ NullifierHash₂ Root₁ Root₂ : F)
     (Path₁ Proof₁ Path₂ Proof₂: Vector F 20)
